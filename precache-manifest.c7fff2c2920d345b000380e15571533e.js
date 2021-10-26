4c07baa29d21d61a8823e8fea98e56c11d69f1a0
hamza.mahmood@funavry.com-at-890242958078 <hamza.mahmood@funavry.com-at-890242958078>
2021-10-26 23:21:28 +0500

    asd

---
+++ precache-manifest.c7fff2c2920d345b000380e15571533e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "752cc784d53eee75d94001cd97c0be68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "143bc2c47955cba515e2",
+    "url": "/static/css/main.ad4494ce.chunk.css"
+  },
+  {
+    "revision": "96a8611fa1dd336f5c7a",
+    "url": "/static/js/2.bdb7330d.chunk.js"
+  },
+  {
+    "revision": "4c0c83c372d3b6e92da93dcb6e2c075e",
+    "url": "/static/js/2.bdb7330d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "143bc2c47955cba515e2",
+    "url": "/static/js/main.376e4c27.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
